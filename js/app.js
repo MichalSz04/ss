05136988a8fb8c14eb003f387ed37b613a8c4b31
unknown <klnb2014@gmail.com>
2022-07-30 20:11:34 +0200

    dodano sekcje dlaczego, usprawniono dzialanie menu wysuwanego

--- js/app.js
+++ js/app.js
@@ -23,17 +23,47 @@ const navSlide = () => {
 		burger.classList.toggle('toggleX');
 	});
 };
+const closeMenu = () => {
+	const burger = document.querySelector('.navbar__logo');
+	const navbar = document.querySelector('.navbar__list');
+	const body = document.querySelector('body');
+	const links = document.querySelectorAll('.navbar__links');
+	body.classList.toggle('body--darken');
+	navbar.classList.toggle('navbar__list--active');
+	links.forEach((link, index) => {
+		if (link.style.animation) {
+			link.style.animation = '';
+		} else {
+			link.style.animation = `navLinksFade 0.5s ease forwards ${
+				index / 7 + 0.4
+			}s`;
+		}
+	});
+	burger.classList.toggle('toggleX');
 
-const main = () => {
-	navSlide();
-};
-main();
+}
 
-const footerYear = document.querySelector('.footer__year');
+const menuExit = () => {
+	const mediaQuery = window.matchMedia('(max-width: 968px)')
+	if (mediaQuery.matches){
+	const menuElements = document.querySelectorAll('.navbar__links');
+	menuElements.forEach((menuElement) =>{
+		menuElement.addEventListener('click',()=>{
+			closeMenu();
+		})
+	});}
+}
 
 const handleCurrentYear = () => {
+	const footerYear = document.querySelector('.footer__year');
 	const year = new Date().getFullYear();
 	footerYear.innerText = year;
 };
 
-handleCurrentYear();
+
+const main = () => {
+	navSlide();
+	handleCurrentYear();
+	menuExit();
+};
+main();
